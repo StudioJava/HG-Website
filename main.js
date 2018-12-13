83b3cebf47b034d3e7f465b6f5139f3e021d6d1f
studiojava <ejrojas@icloud.com>
2018-12-13 12:13:41 -0400

    udate on animation

---
+++ main.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+TweenMax.to("#logo", 5, {x:200,rotation:360,scale:0.5,delay:3,backgroundColor:"green",padding:20});
+TweenMax.from("#logo", 2, {opacity:0,scale:0,ease:Bounce.easeOut});
+
+//alert("hey");
